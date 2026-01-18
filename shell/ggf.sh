8fba741ff012207585950454349c1d572518c282
sunadarake <sunadarake9@gmail.com>
2026-01-18 15:06:57 +0900

    ggfの引数を受け取らないようにした。よくggcと間違えてしまうため

--- shell/ggf.sh
+++ shell/ggf.sh
@@ -5,8 +5,10 @@ if ! command -v git >/dev/null 2>&1; then
   exit 1
 fi
 
-if [ $# -eq 0 ]; then
-  git pull origin  main
-else
-  git pull "$@"
+if [ $# -ne 0 ]; then
+  echo "ERROR: ggf は引数を受け取りません"
+  echo "もしかして ggc と間違えていませんか？"
+  exit 1
 fi
+
+git pull origin main
