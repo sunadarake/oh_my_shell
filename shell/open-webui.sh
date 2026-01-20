957e0c7450d659557cd9d67b31d34ec1ae815494
sunadarake <sunadarake9@gmail.com>
2026-01-21 08:05:25 +0900

    shell/open-webui.shでhome_dirに移動する処理を追加。

--- shell/open-webui.sh
+++ shell/open-webui.sh
@@ -6,6 +6,8 @@ if ! command -v uvx > /dev/null 2>&1; then
     exit 1
 fi
 
+cd "$HOME" || exit 1
+
 case "$(uname -s)" in
     Linux*|Darwin*)
         DATA_DIR=~/.open-webui uvx --python 3.11 open-webui@latest serve
